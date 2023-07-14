5dc7885adf0092b3f788c39e755938cf74a3fc24
m2tg <monishrvps2008@gmail.com>
2023-07-14 17:34:02 +0530

    Completed the project using diff functions

--- javascript.js
+++ javascript.js
@@ -1,10 +1,7 @@
 let choices = ['Rock', 'Paper', 'Scissor'];
 
-let playerSelection = choices[1];
-let computerSelection = getComputerChoice();
-
-let computerWin = `You Lose! ${computerSelection} beats ${playerSelection}`;
-let playerWin = `You Win! ${playerSelection} beats ${computerSelection}`;
+let computerWins = 0
+  , playerWins = 0; 
 
 function getComputerChoice() {
     return choices[randomise(3)];
@@ -14,24 +11,58 @@ function randomise(num) {
     return Math.floor(Math.random() * num);
 }
 
-function playGame(playerChoice, computerChoice) {
+function playGame(playerChoice, computerChoice,) {
+
+    let computerWin = `You Lose! ${computerChoice} beats ${playerChoice}`;
+    let playerWin = `You Win! ${playerChoice} beats ${computerChoice}`;
+
     if((computerChoice === 'Rock') && (playerChoice === 'Scissor')) {
+        computerWins ++;
         return computerWin;
     }
     else if((computerChoice === 'Paper') && (playerChoice === 'Rock')) {
+        computerWins ++;
         return computerWin;
     }
     else if((computerChoice === 'Scissor') && (playerChoice === 'Paper')) {
+        computerWins ++;
         return computerWin;
     }
     else if(computerChoice === playerChoice){
         return 'Its a Tie! Play again.';
     }
     else {
+        playerWins ++;
         return playerWin;
     }
 }
 
+function decideWinner() {
 
+    if (computerWins > playerWins) {
+        return 'Computer won the game!'; 
+    }
+    else if (playerWins > computerWins) {
+        return 'You win the game';
+    }
+    else {
+        return 'Its a tie! No winners';
+    }
+}
 
-console.log(playGame(playerSelection,computerSelection));
+function capitalise(str) {
+    return (str.charAt(1).toUpperCase()) + str.slice(1);
+}
+
+function game() {
+    for (let i = 1; i<=5; i++) {
+        let playerSelection = capitalise(prompt("What is your play? (Rock / Paper / Scissors"));
+        console.log(playGame(playerSelection, getComputerChoice()));
+    }
+
+    console.log('Game Summary');
+    console.log(`You won ${playerWins} times, the Computer won ${computerWins} times`);
+    console.log(decideWinner());
+}
+
+game();
