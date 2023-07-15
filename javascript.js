78e66db9292d96e1fd4e7e2031ccc93c0fcd99dd
m2tg <monishrvps2008@gmail.com>
2023-07-16 01:55:04 +0530

    Added UI to rock paper scissor project
    Implemented function to work with event listeners on button click to play game and determine game winner after 5 wins.

--- javascript.js
+++ javascript.js
@@ -7,33 +7,51 @@ function getComputerChoice() {
     return choices[randomise(3)];
 }
 
+function startGame (event) {
+    let playerSelection = event.getAttribute('data-value');
+    let messageDiv = document.querySelector('.gameMessage');
+
+    messageDiv.textContent = playGame(playerSelection, getComputerChoice());
+    updateScoreCard();
+    if(computerWins === 5 || playerWins === 5) {    
+        messageDiv.textContent = `Game Over! ${decideWinner()}`;
+        document.querySelectorAll('button').forEach( (element) =>{
+            element.remove();
+        });
+    }
+}
+
+function updateScoreCard(str) {
+    let playerScore = document.querySelector('.playerScore');
+    let computerScore = document.querySelector('.computerScore');
+
+    playerScore.textContent = playerWins;
+    computerScore.textContent = computerWins;    
+}
+
 function randomise(num) {
     return Math.floor(Math.random() * num);
 }
 
 function playGame(playerChoice, computerChoice,) {
 
-    let computerWin = `You Lose! ${computerChoice} beats ${playerChoice}`;
-    let playerWin = `You Win! ${playerChoice} beats ${computerChoice}`;
+    let computerWinMessage = `You Lose! ${computerChoice} beats ${playerChoice}`;
+    let playerWinMessage = `You Win! ${playerChoice} beats ${computerChoice}`;
 
-    if((computerChoice === 'Rock') && (playerChoice === 'Scissor')) {
+    if( (computerChoice === 'Rock') && (playerChoice === 'Scissor') ||
+        (computerChoice === 'Paper') && (playerChoice === 'Rock') ||
+        (computerChoice === 'Scissor') && (playerChoice === 'Paper')) 
+    {
         computerWins ++;
-        return computerWin;
-    }
-    else if((computerChoice === 'Paper') && (playerChoice === 'Rock')) {
-        computerWins ++;
-        return computerWin;
-    }
-    else if((computerChoice === 'Scissor') && (playerChoice === 'Paper')) {
-        computerWins ++;
-        return computerWin;
+        updateScoreCard('c');
+        return computerWinMessage;
     }
     else if(computerChoice === playerChoice){
         return 'Its a Tie! Play again.';
     }
     else {
         playerWins ++;
-        return playerWin;
+        return playerWinMessage;
     }
 }
 
@@ -54,15 +72,10 @@ function capitalise(str) {
     return (str.charAt(1).toUpperCase()) + str.slice(1);
 }
 
-function game() {
-    for (let i = 1; i<=5; i++) {
-        let playerSelection = capitalise(prompt("What is your play? (Rock / Paper / Scissors"));
-        console.log(playGame(playerSelection, getComputerChoice()));
-    }
+const btns = document.querySelectorAll('button');
 
-    console.log('Game Summary');
-    console.log(`You won ${playerWins} times, the Computer won ${computerWins} times`);
-    console.log(decideWinner());
-}
-
-game();
+btns.forEach( (button) => {
+    button.addEventListener('click', function (e){
+        startGame(button);
+    });
+});
